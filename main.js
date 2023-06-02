de74baef8f093422973643554b98a1050693bf14
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-06-02 14:45:59 +0200

    feat: use fingerprint dx team plugins

--- main.js
+++ main.js
@@ -5,17 +5,107 @@ const core = require("@actions/core");
 
 require('debug').enable('semantic-release:*')
 
+const plugins = [
+    [
+        '@semantic-release/commit-analyzer',
+        {
+            'releaseRules': [
+                {
+                    'type': 'build',
+                    'scope': 'deps',
+                    'release': 'patch'
+                },
+                {
+                    'type': 'docs',
+                    'scope': 'README',
+                    'release': 'patch'
+                }
+            ]
+        }
+    ],
+    [
+        '@semantic-release/release-notes-generator',
+        {
+            'preset': 'conventionalCommits',
+            'presetConfig': {
+                'types': [
+                    {
+                        'type': 'feat',
+                        'section': 'Features'
+                    },
+                    {
+                        'type': 'feature',
+                        'section': 'Features'
+                    },
+                    {
+                        'type': 'fix',
+                        'section': 'Bug Fixes'
+                    },
+                    {
+                        'type': 'perf',
+                        'section': 'Performance Improvements'
+                    },
+                    {
+                        'type': 'revert',
+                        'section': 'Reverts'
+                    },
+                    {
+                        'type': 'docs',
+                        'scope': 'README',
+                        'section': 'Documentation'
+                    },
+                    {
+                        'type': 'build',
+                        'scope': 'deps',
+                        'section': 'Build System'
+                    },
+                    {
+                        'type': 'docs',
+                        'section': 'Documentation',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'style',
+                        'section': 'Styles',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'chore',
+                        'section': 'Miscellaneous Chores',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'refactor',
+                        'section': 'Code Refactoring',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'test',
+                        'section': 'Tests',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'build',
+                        'section': 'Build System',
+                        'hidden': true
+                    },
+                    {
+                        'type': 'ci',
+                        'section': 'Continuous Integration',
+                        'hidden': true
+                    }
+                ]
+            }
+        }
+    ],
+];
+
 async function main() {
     try {
         const currentBranch = process.env.GITHUB_HEAD_REF;
         const result = await semanticRelease({
             noCi: true, dryRun: true, branches: [currentBranch, 'main'],
-            "plugins": [
-                ["@semantic-release/commit-analyzer", {
-                    "preset": "angular",
-                }],
-                '@semantic-release/release-notes-generator',
-            ]
+            "plugins": plugins
         });
         if (result) {
             const {nextRelease} = result;
