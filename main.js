e6d505fe4b1231a0a4d5d20e90046a81351a06c6
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-06-02 13:05:46 +0200

    chore: keep full branch name

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require('debug').enable('semantic-release:*')
 
 async function main() {
     try {
-        const currentBranch = github.context.ref.replace('refs/', '');
+        const currentBranch = github.context.ref;
         console.log(currentBranch, github.context.ref);
         const result = await semanticRelease({
             noCi: true, dryRun: true, branches: [currentBranch, 'main'],
